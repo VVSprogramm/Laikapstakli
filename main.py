be5e4dc4813bd527deb561c706d9f6344c6edda7
Programmēšana VVS <>
2021-05-19 10:21:54 +0000

    Piekļuve konkrētu pilsētu datiem

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ import requests
 API_KEY = "05361bc22fa83fe94fc8552d76d81c44"
 URL = "http://api.weatherstack.com/current"
 
-pilseta = "London"
+pilsetas = []
 
-parametri = {'access_key': API_KEY, 'query': pilseta}
+with open("pilsetas.txt") as f:
+  for line in f:
+    pilsetas.append(line.strip())
+print(pilsetas)
 
-atbilde = requests.get(URL, parametri)
-js = atbilde.json()
-print(js)
-print()
 
-temp = js['current']['temperature']
-datums = js['location']['localtime']
-pilsetaa =js['location']['name']
-valsts = js['location']['country']
+for pilseta in pilsetas:
+  parametri = {'access_key': API_KEY, 'query': pilseta}
+  atbilde = requests.get(URL, parametri)
+  js = atbilde.json()
 
-print(f"Temperatūra {pilsetaa}, {valsts}. {datums} ir {temp} grādi pēc celsija.")
+  temp = js['current']['temperature']
+  datums = js['location']['localtime']
+
+  with open(f"{pilseta}.txt", "w") as f:
+    f.write(f"{datums}, {temp}\n")
+
+
+#1. versija
+# pilseta = "London"
+# parametri = {'access_key': API_KEY, 'query': pilseta}
+
+# atbilde = requests.get(URL, parametri)
+# js = atbilde.json()
+# print(js)
+# print()
+
+# temp = js['current']['temperature']
+# datums = js['location']['localtime']
+# pilsetaa =js['location']['name']
+# valsts = js['location']['country']
+
+# print(f"Temperatūra {pilsetaa}, {valsts}. {datums} ir {temp} grādi pēc celsija.")
